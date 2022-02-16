8fe4c2a3b1136bb1000767eb6e36eac4003c5826
Alexander Terzi <sanyagts@gmail.com>
2022-02-16 18:53:01 +0200

    css fix

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -28,43 +28,44 @@ const Card = ({ id,
   }
 
   return (
-    <div className={css.card}>
-      {
-        loading ?
-          <ContentLoader
-            speed={2}
-            width={155}
-            height={250}
-            viewBox="0 0 150 265"
-            backgroundColor="#f3f3f3"
-            foregroundColor="#ecebeb"
-          >
-            <rect x="0" y="0" rx="10" ry="10" width="150" height="112" />
-            <rect x="0" y="187" rx="10" ry="10" width="100" height="15" />
-            <rect x="0" y="167" rx="10" ry="10" width="155" height="15" />
-            <rect x="0" y="234" rx="10" ry="10" width="80" height="25" />
-            <rect x="117" y="230" rx="10" ry="10" width="32" height="32" />
-          </ContentLoader> :
-          <>
-            {onFavoriteClick && <div className={css.favorite} onClick={hahdleFavoriteClick}>
-              <img src={addToFavorite ? "img/liked.svg" : "img/unliked.svg"} alt="unliked" />
-            </div>}
-            <img width={133} height={112} src={image} alt="Shoes" />
-            <h5>{title}</h5>
-            <div className="d-flex justify-between align-center">
-              <div className="d-flex flex-column">
-                <span>Цена:</span>
-                <b>{price} грн.</b>
+    <div className={css.cardWrap}>
+      <div className={css.card}>
+        {
+          loading ?
+            <ContentLoader
+              speed={2}
+              width={155}
+              height={250}
+              viewBox="0 0 150 265"
+              backgroundColor="#f3f3f3"
+              foregroundColor="#ecebeb"
+            >
+              <rect x="0" y="0" rx="10" ry="10" width="150" height="112" />
+              <rect x="0" y="187" rx="10" ry="10" width="100" height="15" />
+              <rect x="0" y="167" rx="10" ry="10" width="155" height="15" />
+              <rect x="0" y="234" rx="10" ry="10" width="80" height="25" />
+              <rect x="117" y="230" rx="10" ry="10" width="32" height="32" />
+            </ContentLoader> :
+            <>
+              {onFavoriteClick && <div className={css.favorite} onClick={hahdleFavoriteClick}>
+                <img src={addToFavorite ? "img/liked.svg" : "img/unliked.svg"} alt="unliked" />
+              </div>}
+              <img width={133} height={112} src={image} alt="Shoes" />
+              <h5>{title}</h5>
+              <div className="d-flex justify-between align-center">
+                <div className="d-flex flex-column">
+                  <span>Цена:</span>
+                  <b>{price} грн.</b>
+                </div>
+                {onPlusClick && <img
+                  className={css.plus}
+                  src={isItemAdded(id) ? "img/btn-checked.svg" : "img/btn-plus.svg"}
+                  alt="plus"
+                  onClick={handleClickPlus} />}
               </div>
-              {onPlusClick && <img
-                className={css.plus}
-                src={isItemAdded(id) ? "img/btn-checked.svg" : "img/btn-plus.svg"}
-                alt="plus"
-                onClick={handleClickPlus} />}
-            </div>
-          </>
-      }
-
+            </>
+        }
+      </div>
     </div>
   );
 }
